67893068f8ed098e961c5a2eb10508ae34b59335
Nikitat0 <nikitatzero@yandex.ru>
2023-11-13 13:47:04 +0700

    Cleanup mini11 (cursed version)

--- mini11_cursed.py
+++ mini11_cursed.py
@@ -5,12 +5,12 @@ def singleton(_class):
     _class.__instance__ = None
 
     @wraps(_class)
-    def f(*args):
+    def get_instance(*args):
         if _class.__instance__ is None:
             _class.__instance__ = _class(*args)
         return _class.__instance__
 
-    return f
+    return get_instance
 
 
 @singleton
@@ -24,7 +24,6 @@ class Counter():
     def get(self):
         return self.value
 
-print(Counter.__init__)
 Counter(2).inc()
 Counter(2).inc()
 assert Counter().get() == 4
